334ed649bb0204e1467d0db23dbf3e2711085fd9
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2016-11-28 17:52:36 +0900

    국민대학교 교내 경진대회 Open Contest J번 문제

--- 13422.cpp
+++ 13422.cpp
@@ -1,9 +1,25 @@
-//
-//  13422.cpp
-//  algorithm
-//
-//  Created by Seungwoo Kang on 2016. 11. 28..
-//  Copyright © 2016년 Main. All rights reserved.
-//
-
 #include <stdio.h>
+int main(){
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        int N,M,ans=0,K,sum=0;
+        scanf("%d %d %d",&N,&M,&K);
+        int h[200000];
+        for(int i=0; i<N; i++) scanf("%d",&h[i]);
+        
+        for(int i=0; i<N; i++) h[i+N]=h[i];
+        for(int i=0; i<M; i++) sum+=h[i];
+        if(sum < K) ans++;
+        for(int i=0; i<N-1; i++){
+            sum=sum-h[i]+h[(i+M)%N];
+            if(sum < K) ans++;
+        }
+        if(N==M) {
+            if(ans!=0) printf("1\n");
+            else printf("%d\n",ans);
+            continue;
+        }
+        printf("%d\n",ans);
+    }
+}
