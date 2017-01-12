51caa3c2dc22a18a91495338c658b9c79d49a83e
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-01-12 17:27:07 +0900

    계산문제

--- 1009.cpp
+++ 1009.cpp
@@ -1,9 +1,18 @@
-//
-//  1009.cpp
-//  algorithm
-//
-//  Created by Seungwoo Kang on 2017. 1. 12..
-//  Copyright © 2017년 Main. All rights reserved.
-//
-
 #include <stdio.h>
+#include <cmath>
+using namespace std;
+int main(){
+    int T;
+    scanf("%d",&T);
+    for(int testcase=1; testcase<=T; testcase++){
+        long a,b;
+        int data=1;
+        scanf("%ld %ld",&a, &b);
+        for(int i=1; i<=b; i++){
+            data *= a;
+            data %= 10;
+        }
+        printf("%d\n",data==0?10 :data);
+    }
+    return 0;
+}
