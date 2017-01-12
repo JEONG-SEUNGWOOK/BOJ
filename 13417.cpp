f10f4e9d486d15971266b0f265417aacf392ecd6
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-01-12 17:32:05 +0900

    DFS

--- 13417.cpp
+++ 13417.cpp
@@ -1,9 +1,29 @@
-//
-//  13417.cpp
-//  algorithm
-//
-//  Created by Seungwoo Kang on 2016. 11. 28..
-//  Copyright © 2016년 Main. All rights reserved.
-//
 
-#include <stdio.h>
+#include <cstdio>
+#include <deque>
+using namespace std;
+int main(){
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        deque<char> dq;
+        dq.clear();
+        int N;
+        char c;
+        scanf("%d",&N);
+        for(int i=0;i<N;i++){
+            scanf(" %c",&c);
+            if(dq.empty()) dq.push_back(c);
+            else{
+                if(dq.front() >= c) dq.push_front(c);
+                else dq.push_back(c);
+            }
+        }
+        while(!dq.empty()){
+            printf("%c",dq.front());
+            dq.pop_front();
+        }
+        printf("\n");
+    }
+}
+
