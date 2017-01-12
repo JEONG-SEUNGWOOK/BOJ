61bc71fb7c28968b0ff7d856e049a6e969c4e570
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-01-12 17:36:30 +0900

    제 12회 총장배 서강대학교 프로그래밍 대회 Open Contest D번 문제

--- 13903.cpp
+++ 13903.cpp
@@ -1,9 +1,58 @@
-//
-//  13903.cpp
-//  algorithm
-//
-//  Created by Seungwoo Kang on 2016. 12. 1..
-//  Copyright © 2016년 Main. All rights reserved.
-//
 
-#include <stdio.h>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct pos{
+    int r,c,cnt=0;
+};
+
+queue<pos> q;
+int R,C,N,ans;
+int map[1001][1001];
+int isVisited[1001][1001];
+int dr[10], dc[10];
+bool flag = false;
+
+int main(){
+    cin>>R>>C;
+    for(int i=0; i<R;i++)
+        for(int j=0; j<C;j++)
+            cin>>map[i][j];
+    cin>>N;
+    for(int i=0;i<N;i++)
+        cin>>dr[i]>>dc[i];
+    
+    for(int i=0;i<C;i++)
+        if(map[0][i]) {
+            pos p;
+            p.r=0, p.c=i, p.cnt=0;
+            q.push(p);
+            isVisited[0][i]=1;
+        }
+    
+    while(!q.empty()){
+        pos p;
+        p=q.front(); q.pop();
+        
+        if(p.r == R-1){
+            flag=1;
+            ans=p.cnt;
+            break;
+        }
+        for(int i=0; i<N; i++){
+            int nr = p.r+dr[i], nc = p.c+dc[i], nCnt=p.cnt+1;
+            if(nr>=0 && nc>=0 && nr<R && nc<C && !isVisited[nr][nc] && map[nr][nc]){
+                pos t;
+                t.r=nr, t.c=nc, t.cnt=nCnt;
+                q.push(t);
+                isVisited[nr][nc]=1;
+                
+            }
+        }
+        
+    }
+    if(flag) cout<<ans<<"\n";
+    else cout<<-1<<"\n";
+    
+}
