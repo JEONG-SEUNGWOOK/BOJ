b963b4371fe6b563c186ccaf038e2e6c5cf280cd
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-01-12 17:34:38 +0900

    국민대학교 교내 경시대회 Open Contest H번 문제

--- 13420.cpp
+++ 13420.cpp
@@ -1,9 +1,20 @@
-//
-//  13420.cpp
-//  algorithm
-//
-//  Created by Seungwoo Kang on 2016. 11. 28..
-//  Copyright © 2016년 Main. All rights reserved.
-//
-
+/*
 #include <stdio.h>
+
+int main(){
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        long n1, n2,n;
+        char c,e;
+        scanf("%ld %c %ld %c %ld",&n1,&c,&n2,&e,&n);
+        long ans=0;
+        if(c=='+') ans=n1+n2;
+        else if(c=='-') ans=n1-n2;
+        else if(c=='*') ans=n1*n2;
+        else if(c=='/') ans=n1/n2;
+        if(ans==n) printf("correct\n");
+        else printf("wrong answer\n");
+    }
+}
+*/
