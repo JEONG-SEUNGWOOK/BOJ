163c814cf149d4b1ca0215ffc2428f850ab840f2
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-03-01 03:46:08 +0900

    counting sort

--- 10989.cpp
+++ 10989.cpp
@@ -1,25 +1,20 @@
 #include <stdio.h>
 
-#define MAX_N 10000000
 #define MAX_SIZE 10000
-int N,A[MAX_SIZE+1],B[MAX_SIZE+1],count[MAX_N+1]={0,}, sum[MAX_N+1];
+int N,a,count[MAX_SIZE+1]={0,}, sum[MAX_SIZE+1];
 
 int main(){
     scanf("%d",&N);
     
-    for(int i =1 ; i<= N ; i++){
-        scanf("%d", &A[i]);
-        count[A[i]]++;
+    for(int i=1; i<=N ; i++){
+        scanf("%d", &a);
+        count[a]++;
     }
     
-    sum[0] = count[0];
-    for(int i = 1 ; i<= MAX_N ; i++)
-        sum[i] = sum[i-1]+count[i];
-    
-    for(int i=N;i>=1;i--){
-        B[sum[A[i]]] = A[i];
-        sum[A[i]]--;
+    for(int i=1;i<=MAX_SIZE;i++){
+        while(count[i]-- > 0){
+            printf("%d\n",i);
+        }
     }
-    for(int i=1;i<=N;i++)
-        printf("%d\n",B[i]);
+        
 }
