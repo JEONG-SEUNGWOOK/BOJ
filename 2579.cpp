0c8c2996f41cb0e58e3b7189328d1b02e2e39452
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-03-09 23:11:37 +0900

    DP

--- 2579.cpp
+++ 2579.cpp
@@ -1,28 +1,17 @@
 
-#include <stdio.h>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
 
-int N;
-int step[310];
-int point[2][310]={0,};
-
-void input(){
-    scanf("%d", &N);
-    for(int i=0; i<N; i++){
-        scanf("%d", &step[i]);
-    }
-}
-
-void dp(){
-    point[0][0] = point[1][0] = step[0];
-    for(int i=1; i<=N; i++){
-        point[0][i] += point[1][i-1];
-        point[1][i] += point[0][i-1];
-    }
-}
-void output(){
-}
 int main(){
-    input();
-    dp();
-    output();
+    int N, step[301], sum[301]={0,};
+    scanf("%d",&N);
+    for(int i=1;i<=N;i++)
+        scanf("%d",&step[i]);
+    
+    sum[1]= step[1];
+    for(int i=2;i<=N;i++){
+        sum[i] = max(step[i-1] + sum[i-3], sum[i-2])+step[i];
+    }
+    printf("%d\n",sum[N]);
 }
