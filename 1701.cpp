c4fd86a9df9a3075060950f1d5286abb71812275
JEONG-SEUNGWOOK <zuoi71@gmail.com>
2017-03-18 15:46:27 +0900

    suffix array

--- 1701.cpp
+++ 1701.cpp
@@ -1,35 +1,25 @@
-/*
 
+
+#include <string>
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
 using namespace std;
 
-int ans=0,cnt=0;
-string s;
-vector<string> suf;
 int main(){
-    cin >> s;
-    for (int i = 0; i < s.size(); ++i) {
-        string temp;
-        for(int j=i;j<s.size();j++) temp += s.at(j);
-        suf.push_back(temp);
-    }
+    string s;
+    vector<string> v;
+    int cnt=0,ans=0;
+    cin>>s;
+    for(int i=0;i<s.size();i++)
+        v.push_back(s.substr(i,s.size()));
     
-    sort(suf.begin(), suf.end());
+    sort(v.begin(), v.end());
     
-    
-    for(vector<string>::iterator iter = suf.begin(); iter != suf.end(); iter++){
-        cout<<*iter<<endl;
-    }
-    
-    for (int i = 0; i < suf.size() - 1; ++i) {
-        while (suf[i][cnt] == suf[i + 1][cnt]) ++cnt;
-        ans = max(ans, cnt); cnt = 0;
+    for(int i=0;i<v.size()-1;i++){
+        while(v[i][cnt] == v[i+1][cnt]) cnt++;
+        ans = max(cnt, ans);
+        cnt=0;
     }
     cout<<ans<<"\n";
-    
-    
 }
-*/
